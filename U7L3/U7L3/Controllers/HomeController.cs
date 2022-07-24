cafe2a100dff5e6baaeb665575929d4d7f12cd5d
amandabledsoe <amandajbledsoe@gmail.com>
2022-07-24 00:51:52 -0400

    Needs some formatting work but we are getting a response for both Shuffle the Deck and Draw A Card API Calls!

--- U7L3/U7L3/Controllers/HomeController.cs
+++ U7L3/U7L3/Controllers/HomeController.cs
@@ -27,7 +27,7 @@ namespace U7L3.Controllers
 
         public IActionResult ShuffleTheCards() // Same as Generate a New Deck
         {
-            //1. First, create an HTTP Client to enable the ability send ab receive a response to the API:
+            //1. First, create an HTTP Client via the Client Factory to enable the ability send ab receive a response to the API:
             HttpClient httpClient = _httpClientFactory.CreateClient();
 
             //2. Second, find the URL to call the API, and set it as a constant for use later.
@@ -47,6 +47,37 @@ namespace U7L3.Controllers
 
             //Finally, pass the API Response Model instance to the view so we can see what what are working with!
             return View(displayShuffledDeck);
+        }
+
+        public IActionResult DrawACardAction(string deck_id)
+        {
+            //1. First, create an HTTP Client via the Client Factory to enable the ability send ab receive a response to the API:
+            HttpClient httpClient = _httpClientFactory.CreateClient();
+
+            //2. Second, find the URL to call the API, and set it as a constant for use later.
+            //https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=2
+
+            //3. See if you have to send the API anything. In this case, we need to give it a deck_id and a count.
+            //For now, since we are setting the amount of cards to draw, we can assign that here and pass it in
+            int cardCount = 5;
+
+            //We already pass in a deck_id, so now we just plug them both in and set the URL to a variable instead of a constant.
+            string apiURLRequestToDrawACard = $"https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count={cardCount}";
+
+
+            //4. Create a Model that models (haha) the returned response from the API. 
+            //See 'DrawACardAction.cs'
+
+            //5. Call the API using the GetFromJasonAsync method attached to the HTTP Client. 
+            //Dont forget to add GetAwaiter() and GetResult()
+            var drawACardAPIResponse = httpClient.GetFromJsonAsync<DrawACard>(apiURLRequestToDrawACard).GetAwaiter().GetResult();
+
+            //6. Create a new instance of the API Response Model and store the API response you get into that instance.
+            var cardsToDisplay = drawACardAPIResponse;
+
+            //Finally, pass the API Response Model instance to the view so we can see what what are working with!
+            return View(cardsToDisplay);
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ U7L3/U7L3/Models/DrawACard.cs
@@ -0,0 +1,24 @@
+﻿namespace U7L3.Models
+{
+    public class DrawACardFullResponse
+    {
+        public DrawACard deckDetails { get; set; }
+        public DrawACard_CardDetails[] cardDetails { get; set; }
+
+    }
+    public class DrawACard
+    {
+        public bool success { get; set; }
+        public string deck_id { get; set; }
+        public int remaining { get; set; }
+        public DrawACard_CardDetails[] cards { get; set; }
+
+    }
+    public class DrawACard_CardDetails
+    {
+        public string image { get; set; }
+        public string value { get; set; }
+        public string suit { get; set; }
+        public string code { get; set; }
+    }
+}
